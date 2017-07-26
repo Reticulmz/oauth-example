2fa1fdc15eebe8218b69c64e0e4fdcbc7f216369
Morgan Bazalgette <the@howl.moe>
2017-07-26 18:15:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$provider = new \League\OAuth2\Client\Provider\GenericProvider([
+    'clientId'                => "CLIENT ID",     // Create your client on ripple.moe/dev/apps
+    'clientSecret'            => "CLIENT SECRET",
+    'redirectUri'             => 'http://your-website.com/',
+
+    'urlAuthorize'            => 'http://hanayo.org/oauth/authorize',
+    'urlAccessToken'          => 'http://hanayo.org/oauth/token',
+    'urlResourceOwnerDetails' => 'http://hanayo.org/api/v1/ping',
+
+    'scopeSeparator'          => ' ',
+    'scopes'                  => ['read_confidential'],
+]);
+
+// If there is an error, show it
+if (isset($_GET["error"])) {
+    echo "error: $_GET[error]; error_description: $_GET[error_description]";
+    return;
+}
+
+if (!isset($_GET['code'])) {
+
+    // Fetch the authorization URL from the provider; this returns the
+    // urlAuthorize option and generates and applies any necessary parameters
+    // (e.g. state).
+    $authorizationUrl = $provider->getAuthorizationUrl();
+
+    // Get the state generated for you and store it to the session.
+    $_SESSION['oauth2state'] = $provider->getState();
+
+    // Redirect the user to the authorization URL.
+    header('Location: ' . $authorizationUrl);
+    exit;
+
+// Check given state against previously stored one to mitigate CSRF attack
+} elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+
+    if (isset($_SESSION['oauth2state'])) {
+        unset($_SESSION['oauth2state']);
+    }
+
+    exit('Invalid state');
+
+} else {
+
+    try {
+
+        // Try to get an access token using the authorization code grant.
+        $accessToken = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+
+        // We have an access token, which we may use in authenticated
+        // requests against the service provider's API.
+        echo 'Access Token: ' . $accessToken->getToken() . "<br>";
+
+        // Using the access token, we may look up details about the
+        // resource owner.
+        $resourceOwner = $provider->getResourceOwner($accessToken);
+
+        var_export($resourceOwner->toArray());
+
+    } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+        // Failed to get the access token or user details.
+        exit($e->getMessage());
+
+    }
+
+}
